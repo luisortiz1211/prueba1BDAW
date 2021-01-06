820280044ab35d2f29639899275de606a6003bca
luisortiz1211 <66312625+luisortiz1211@users.noreply.github.com>
2021-01-06 12:22:26 -0500

    #9 Auth jwt

--- routes/api.php
+++ routes/api.php
@@ -43,4 +43,15 @@ use Illuminate\Http\Request;
     Route::delete('articles/{article}', 'ArticleController@delete');
     //Route::delete('articles/{id}', 'ArticleController@delete');
 
+    Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@authenticate');
+    Route::get('articles', 'ArticleController@index');
+
+    Route::group(['middleware' => ['jwt.verify']], function() {
+        Route::get('user', 'UserController@getAuthenticatedUser');
+        Route::get('articles/{article}', 'ArticleController@show');
+        Route::post('articles', 'ArticleController@store');
+        Route::put('articles/{article}', 'ArticleController@update');
+        Route::delete('articles/{article}', 'ArticleController@delete');
+        });
 
